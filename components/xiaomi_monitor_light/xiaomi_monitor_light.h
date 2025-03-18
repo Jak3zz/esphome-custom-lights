030243821b4fac8dbaa9abec385e4699bf1bcd47
Jarkko Uusitalo <jarkko.j.uusitalo@gmail.com>
2025-03-19 00:07:30 +0200

    Fix dependency in xiaomi component

--- components/xiaomi_monitor_light/xiaomi_monitor_light.h
+++ components/xiaomi_monitor_light/xiaomi_monitor_light.h
@@ -2,7 +2,7 @@
 
 #include "esphome/core/component.h"
 #include "esphome/components/light/light_output.h"
-#include <NeoPixelBus.h>
+#include <Arduino.h>
 
 namespace esphome {
 namespace xiaomi_monitor_light {
